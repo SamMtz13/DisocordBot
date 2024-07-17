010fe8a9ce711ac9f027789c71de33e0f3e207a1
Alan Samuel Martinez Alonso <leumas.mart26@gmail.com>
2024-07-16 23:46:12 -0600

    feat: add level xp

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 const { Client, IntentsBitField, EmbedBuilder} = require('discord.js');
 const eventHandler = require('./handlers/eventHandler');
+const mongoose = require('mongoose');
 
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ],
-});
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildPresences,
+    IntentsBitField.Flags.MessageContent,
+]});
 
 
 
@@ -126,6 +127,18 @@ client.on('interactionCreate', async (interaction) => {
 }
 );
 
-eventHandler(client);
+(async () => {
+    try {
+      mongoose.set('strictQuery', false);
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log('Connected to DB.');
+  
+      
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  })();
+  eventHandler(client);
+
 
 client.login(process.env.TOKEN);
